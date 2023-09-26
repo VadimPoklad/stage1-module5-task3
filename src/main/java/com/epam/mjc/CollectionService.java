a4f219b7364539b9866964e385af10e886b4bbbd
Vadym Poklad <vadik.pokladd@gmail.com>
2023-09-27 00:20:08 +0300

    Answ

---
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -0,0 +1,37 @@
+package com.epam.mjc;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class CollectionService {
+
+    public List<Integer> filterEvenNumbers(List<Integer> list) {
+        return list.stream()
+                .filter(o1 -> o1%2==0)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> toUpperCaseCollection(List<String> list) {
+        return list.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Integer> findMax(List<Integer> list) {
+        return list.stream()
+                .max(Integer::compareTo);
+    }
+
+    public Optional<Integer> findMin(List<List<Integer>> list) {
+        return list.stream()
+                .flatMap(Collection::stream)
+                .min(Integer::compareTo);
+    }
+
+    public Integer sum(List<Integer> list) {
+        return list.stream()
+                .reduce(0,Integer::sum);
+    }
+}
